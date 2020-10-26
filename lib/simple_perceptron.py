d07f1872973fee48cf137bb7038d8d187fe579d1
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2020-10-26 15:50:51 +0900

    Refactor with weight and bias

--- lib/simple_perceptron.py
+++ lib/simple_perceptron.py
@@ -1,33 +1,35 @@
+import numpy as np
+
 class SimplePerceptron:
     def __init__(self):
         pass
 
     def and_gate(self, x1, x2):
-        w1    = 0.5
-        w2    = 0.5
-        theta = 0.7
-        tmp = (x1 * w1) + (x2 * w2)
-        if tmp > theta:
+        x = np.array([x1, x2])
+        w = np.array([0.5, 0.5])
+        b = -0.7
+        tmp = np.sum(x * w) + b
+        if tmp > 0:
             return 1
         else:
             return 0
 
     def nand_gate(self, x1, x2):
-        w1    = -0.5
-        w2    = -0.5
-        theta = -0.7
-        tmp = (x1 * w1) + (x2 * w2)
-        if tmp > theta:
+        x = np.array([x1, x2])
+        w = np.array([-0.5, -0.5])
+        b = 0.7
+        tmp = np.sum(x * w) + b
+        if tmp > 0:
             return 1
         else:
             return 0
 
     def or_gate(self, x1, x2):
-        w1    = 0.8
-        w2    = 0.8
-        theta = 0.7
-        tmp = (x1 * w1) + (x2 * w2)
-        if tmp > theta:
+        x = np.array([x1, x2])
+        w = np.array([0.8, 0.8])
+        b = -0.7
+        tmp = np.sum(x * w) + b
+        if tmp > 0:
             return 1
         else:
             return 0
