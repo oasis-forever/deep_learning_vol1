41f984ee2194c2f6de7a447e59a3d401f9b080c3
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-01-01 17:45:04 +0900

    Update NeuralNetworkLearning#cross_entropy_error for mini batch process

--- lib/neural_network_learning.py
+++ lib/neural_network_learning.py
@@ -24,5 +24,12 @@ class NeuralNetworkLearning:
         return 0.5 * np.sum((y - t) ** 2)
 
     def cross_entropy_error(self, y, t):
+        if y.dim == 0:
+            t = t.reshape(1, t.size)
+            y = y.reshape(1, y.size)
+        batch_size = y.shape[0]
         delta = 1e-7
-        return -np.sum(t * np.log(y + delta))
+        # One-hot
+        return -np.sum(t * np.log(y + delta)) / batch_size
+        # Non One-hot
+        # return -np.sum(t * np.log(y[np.arrage(batch_size), t] + delta)) / batch_size
