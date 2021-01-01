2dea96a019c13cbf656328e48be3912c81fe46aa
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-01-01 17:43:31 +0900

    Commit NetworkLearning#_get_train_data

--- lib/neural_network_learning.py
+++ lib/neural_network_learning.py
@@ -1,7 +1,15 @@
 import numpy as np
+import os, sys
+sys.path.append(os.pardir)
+from dataset.mnist import load_mnist
+
 class NeuralNetworkLearning:
     def __init__(self):
         pass
+
+    def _get_train_data(self):
+        (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, one_hot_label=True)
+        return x_train, t_train
 
     def mean_squared_error(self, y, t):
         return 0.5 * np.sum((y - t) ** 2)
