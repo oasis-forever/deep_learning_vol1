ee5cd538a4a6d07ff0327d680cdd3006fa8e4d0e
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-01-01 17:44:27 +0900

    Commit NetworkLearning#_random_choice

--- lib/neural_network_learning.py
+++ lib/neural_network_learning.py
@@ -11,6 +11,15 @@ class NeuralNetworkLearning:
         (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, one_hot_label=True)
         return x_train, t_train
 
+    def _random_choice(self):
+        x_train, t_train = self._get_train_data()
+        train_size = x_train.shape[0]
+        batch_size = 10
+        batch_mask = np.random.choice(train_size, batch_size)
+        x_batch = x_train[batch_mask]
+        t_batch = t_train[batch_mask]
+        return x_batch, t_batch
+
     def mean_squared_error(self, y, t):
         return 0.5 * np.sum((y - t) ** 2)
 
