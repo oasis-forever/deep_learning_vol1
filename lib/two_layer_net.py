57d4e597864033a97c0388a7a58a15561ea08a04
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-01-06 10:37:40 +0900

    Update TwoLayerNet with backpropagation

--- lib/two_layer_net.py
+++ lib/two_layer_net.py
@@ -1,34 +1,36 @@
 import numpy as np
 import sys
 sys.path.append("./concerns")
-from functions import *
+from layers import *
 from gradient import numerical_gradient
+from collections import OrderedDict
 
 class TwoLayerNet:
     def __init__(self, input_size, hidden_size, output_size, weight_unit_std=0.01):
         self.params = {}
-        self.params["W1"] = weight_unit_std * np.random.randn(input_size, hidden_size)
-        self.params["b1"] = np.zeros(hidden_size)
-        self.params["W2"] = weight_unit_std * np.random.randn(hidden_size, output_size)
-        self.params["b2"] = np.zeros(output_size)
+        self.params["W1"]      = weight_unit_std * np.random.randn(input_size, hidden_size)
+        self.params["b1"]      = np.zeros(hidden_size)
+        self.params["W2"]      = weight_unit_std * np.random.randn(hidden_size, output_size)
+        self.params["b2"]      = np.zeros(output_size)
+        self.layers            = OrderedDict()
+        self.layers["Affine1"] = Affine(self.params["W1"], self.params["b1"])
+        self.layers["Relu"]    = Relu()
+        self.layers["Affine2"] = Affine(self.params["W2"], self.params["b2"])
+        self.last_layer        = SoftmaxWithLoss()
 
     def predict(self, x):
-        W1, W2 = self.params["W1"], self.params["W2"]
-        b1, b2 = self.params["b1"], self.params["b2"]
-        a1 = np.dot(x, W1) + b1
-        z = sigmoid(a1)
-        a2 = np.dot(z, W2) + b2
-        y = softmax(a2)
-        return y
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        return x
 
     def loss(self, x, t):
         y = self.predict(x)
-        return cross_entropy_error(y, t)
+        return self.last_layer.forward(y, t)
 
     def accuracy(self, x, t):
         y = self.predict(x)
         y = np.argmax(y, axis=1)
-        t = np.argmax(t, axis=1)
+        if t.ndim != 1 : t = np.argmax(t, axis=1)
         accuracy = np.sum(y == t) / float(x.shape[0])
         return accuracy
 
@@ -39,4 +41,21 @@ class TwoLayerNet:
         grads["b1"] = numerical_gradient(loss_W, self.params["b1"])
         grads["W2"] = numerical_gradient(loss_W, self.params["W2"])
         grads["b2"] = numerical_gradient(loss_W, self.params["b2"])
+        return grads
+
+    def gradient(self, x, t):
+        # Forward
+        self.loss(x, t)
+        # Backward
+        dout   = 1
+        dout   = self.last_layer.backward(dout)
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+        grads = {}
+        grads["W1"] = self.layers["Affine1"].dW
+        grads["b1"] = self.layers["Affine1"].db
+        grads["W2"] = self.layers["Affine2"].dW
+        grads["b2"] = self.layers["Affine2"].db
         return grads
